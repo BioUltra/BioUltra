7e1bae2485651888ef71377fbe94ea91f6ab8c52
Vitor Tavares <vitor.tavares.leite@gmail.com>
2024-04-19 22:11:15 -0300

    Add files via upload

---
+++ BioUltra/assets/js/script.js
@@ -0,0 +1,11 @@
+
+function toggle(){ 
+    var nav = document.getElementById("nav")
+    var blur = document.getElementById("blur")
+    var nav1 = document.getElementById("navbar")
+    blur.classList.toggle('active')
+    nav1.classList.toggle('showagain')
+    nav.classList.toggle('show');
+ }
+
+
