c161f9bfd453ca2539a2536af89ca481c367b12b
PrintfFiveo <vitor.tavares.leite@gmail.com>
2024-05-07 18:25:07 -0300

    deleted:    assets/css/sobre.css 	modified:   assets/css/style.css 	modified:   assets/js/script.js 	modified:   index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,51 @@
+const navbar = document.getElementById("navbar");
+const navbarToggle = navbar.querySelector(".navbar-toggle");
 
-function toggle(){ 
-    var nav = document.getElementById("nav")
-    var blur = document.getElementById("blur")
-    var nav1 = document.getElementById("navbar")
-    blur.classList.toggle('active')
-    nav1.classList.toggle('showagain')
-    nav.classList.toggle('show');
- }
+
+function closeMobileNavbar() {
+  navbar.classList.remove("opened");
+  navbarToggle.setAttribute("aria-expanded", "false");
+}
+  function openMobileNavbar() {
+    navbar.classList.add("opened");
+    navbarToggle.setAttribute("aria-expanded", "true");
+  }
+  navbarToggle.addEventListener("click", () => {
+    if (navbar.classList.contains("opened")) {
+      closeMobileNavbar();
+    } else {
+      openMobileNavbar();
+    }
+  });
+  
+  const navbarMenu = navbar.querySelector("#navbar-menu");
+  const navbarLinksContainer = navbar.querySelector(".navbar-links");
+  
+  navbarLinksContainer.addEventListener("click", (clickEvent) => {
+    clickEvent.stopPropagation();
+  });
+  
+  navbarMenu.addEventListener("click", closeMobileNavbar);
+  
+  
 
 
  function limparMain() {
     const mainElement = document.getElementById('main1');
-    const mainElement2 = document.getElementById('main');
-    mainElement2.classList.add('main3');
-      mainElement.classList.add('main2');
+    const mainElement1 = document.getElementById('main2');
+    
+   
+      mainElement.classList.add('opened');
+      mainElement1.classList.add('opened');
    
     }
     
  function voltar() {
     const mainElement = document.getElementById('main1');
-    const mainElement2 = document.getElementById('main');
-    mainElement2.classList.remove('main3');
-      mainElement.classList.remove('main2');
+    const mainElement2 = document.getElementById('main1');
+    mainElement2.classList.remove('opened');
+      mainElement.classList.remove('opened');
      
-    }
-    function login(){
-        const form = document.getElementById('forma');
-
-        form.classList.toggle("forms")
     }
 
     
